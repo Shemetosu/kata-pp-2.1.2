7ca1472cea3648a9a9abf6bf5d59ef7c083229a7
Vladimir Shemetenko <ov.shtk@gmail.com>
2025-03-01 09:18:23 +0300

    Задание выполнил, тесты прошёл

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld helloWorldBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorldBean.getMessage());
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean2.getMessage());
+
+        boolean helloWorldCompare = bean == helloWorldBean;
+        boolean catCompare = catBean == catBean2;
+        System.out.println(helloWorldCompare);
+        System.out.println(catCompare);
     }
 }
